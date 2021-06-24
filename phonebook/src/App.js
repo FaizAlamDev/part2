d07061b75f1bcfcef59729062576e74ab2ba3cc6
faizalam5552 <faizalam9883@protonmail.com>
2021-06-24 21:05:50 +0530

    2.8
    took 15 mins

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -3,19 +3,25 @@ import React, { useState } from 'react'
 const App = () => {
 	const [persons, setPersons] = useState([])
 	const [newName, setNewName] = useState('')
+	const [newNumber, setNewNumber] = useState('')
 
 	const handleName = (e) => {
 		setNewName(e.target.value)
 	}
 
+	const handleNumber = (e) => {
+		setNewNumber(e.target.value)
+	}
+
 	const handleForm = (e) => {
 		e.preventDefault()
-		if (persons.find((person) => person === newName)) {
+		if (persons.find((person) => person.name === newName)) {
 			window.alert(`${newName} is already added to phonebook`)
 		} else {
-			setPersons(persons.concat(newName))
+			setPersons(persons.concat({ name: newName, number: newNumber }))
 		}
 		setNewName('')
+		setNewNumber('')
 	}
 
 	return (
@@ -26,12 +32,17 @@ const App = () => {
 					name: <input value={newName} onChange={handleName} />
 				</div>
 				<div>
+					number: <input value={newNumber} onChange={handleNumber} />
+				</div>
+				<div>
 					<button type='submit'>add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
 			{persons.map((person) => (
-				<p key={person}>{person}</p>
+				<div key={person.name}>
+					{person.name} {person.number}
+				</div>
 			))}
 		</div>
 	)
