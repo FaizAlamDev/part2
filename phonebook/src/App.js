c1497dffbeb94b79f00ce9d6b13315e3acf53cab
Faiz Alam <faizalam9883@gmail.com>
2021-06-24 17:02:16 +0530

    exercise 2.6
    took 2 hours

---
+++ phonebook/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+const App = () => {
+	const [persons, setPersons] = useState([])
+	const [newName, setNewName] = useState('')
+
+	const handleName = (e) => {
+		setNewName(e.target.value)
+	}
+
+	const handleForm = (e) => {
+		e.preventDefault()
+		setPersons(persons.concat(newName))
+		setNewName('')
+	}
+
+	return (
+		<div>
+			<h2>Phonebook</h2>
+			<form onSubmit={handleForm}>
+				<div>
+					name: <input value={newName} onChange={handleName} />
+				</div>
+				<div>
+					<button type='submit'>add</button>
+				</div>
+			</form>
+			<h2>Numbers</h2>
+			{persons.map((person) => (
+				<p key={person}>{person}</p>
+			))}
+		</div>
+	)
+}
+
+export default App
